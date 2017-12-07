66211d73f68bfe8729b7fd9ba8f102570b87d303
Janina Hosch <hoschi-it@web.de>
2017-12-07 13:58:42 +0100

    change output message ...

--- javafx_tutorial/src/javafx_tutorial/Main.java
+++ javafx_tutorial/src/javafx_tutorial/Main.java
@@ -3,7 +3,7 @@ package javafx_tutorial;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from Palle!");
+		System.out.println("Hello World from PillePalle!");
 	}
 
 }
